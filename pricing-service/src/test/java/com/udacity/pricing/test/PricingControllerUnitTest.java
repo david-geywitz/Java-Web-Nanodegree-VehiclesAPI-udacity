5ed769d218c93d51fb97d5b37d3beb48c9622a9f
david-geywitz <56725848+david-geywitz@users.noreply.github.com>
2024-01-30 14:39:11 +0100

    Add files via upload

---
+++ pricing-service/src/test/java/com/udacity/pricing/test/PricingControllerUnitTest.java
@@ -0,0 +1,38 @@
+package com.udacity.pricing.test;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    PricingService priceService;
+
+    @Test
+    public void getVehiclePrice() throws Exception {
+        this.mockMvc.perform(get("/services/price?vehicleId=1")
+        .accept(MediaType.APPLICATION_JSON_UTF8)
+        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        verify(this.priceService, times(1)).getPrice(1L);
+    }
+}
